8dfc44108831a56fc100361f9d328fde037b276d
Erik Moqvist <erik.moqvist@gmail.com>
2019-02-21 07:20:42 +0100

    More tests and statistics.

--- tests/test_detools.py
+++ tests/test_detools.py
@@ -6,33 +6,61 @@ import detools
 
 class DetoolsTest(unittest.TestCase):
 
-    def test_create_patch_foo(self):
+    def assert_create_patch(self, from_filename, to_filename, patch_filename):
         fpatch = BytesIO()
 
-        with open('tests/files/foo.old', 'rb') as fold:
-            with open('tests/files/foo.new', 'rb') as fnew:
+        with open(from_filename, 'rb') as fold:
+            with open(to_filename, 'rb') as fnew:
                 detools.create_patch(fold, fnew, fpatch)
 
         actual = fpatch.getvalue()
 
-        with open('tests/files/foo.patch', 'rb') as fpatch:
+        with open(patch_filename, 'rb') as fpatch:
             expected = fpatch.read()
 
         self.assertEqual(actual, expected)
 
-    def test_apply_patch_foo(self):
+    def assert_apply_patch(self, from_filename, to_filename, patch_filename):
         fnew = BytesIO()
 
-        with open('tests/files/foo.old', 'rb') as fold:
-            with open('tests/files/foo.patch', 'rb') as fpatch:
+        with open(from_filename, 'rb') as fold:
+            with open(patch_filename, 'rb') as fpatch:
                 detools.apply_patch(fold, fpatch, fnew)
 
         actual = fnew.getvalue()
 
-        with open('tests/files/foo.new', 'rb') as fnew:
+        with open(to_filename, 'rb') as fnew:
             expected = fnew.read()
 
         self.assertEqual(actual, expected)
+
+    def assert_create_and_apply_patch(self,
+                                      from_filename,
+                                      to_filename,
+                                      patch_filename):
+        self.assert_create_patch(from_filename, to_filename, patch_filename)
+        self.assert_apply_patch(from_filename, to_filename, patch_filename)
+
+    def test_create_and_apply_patch_foo(self):
+        self.assert_create_and_apply_patch('tests/files/foo.old',
+                                           'tests/files/foo.new',
+                                           'tests/files/foo.patch')
+
+    def test_create_and_apply_patch_foo_backwards(self):
+        self.assert_create_and_apply_patch('tests/files/foo.new',
+                                           'tests/files/foo.old',
+                                           'tests/files/foo-backwards.patch')
+
+    def test_create_and_apply_patch_micropython(self):
+        self.assert_create_and_apply_patch(
+            'tests/files/micropython-esp8266-20180511-v1.9.4.bin',
+            'tests/files/micropython-esp8266-20190125-v1.10.bin',
+            'tests/files/micropython-esp8266-20180511-v1.9.4--20190125-v1.10.patch')
+
+    def test_create_and_apply_patch_no_delta(self):
+        self.assert_create_and_apply_patch('tests/files/foo.new',
+                                           'tests/files/foo.new',
+                                           'tests/files/foo-no-delta.patch')
 
     def test_apply_patch_bad_header_magic(self):
         fnew = BytesIO()
@@ -50,7 +78,7 @@ class DetoolsTest(unittest.TestCase):
         fnew = BytesIO()
 
         with open('tests/files/foo.old', 'rb') as fold:
-            with open('tests/files/empty.patch', 'rb') as fpatch:
+            with open('tests/files/foo-empty.patch', 'rb') as fpatch:
                 with self.assertRaises(detools.Error) as cm:
                     detools.apply_patch(fold, fpatch, fnew)
 
