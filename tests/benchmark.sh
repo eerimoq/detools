bbce35049328a3c657f77b9f808d2c05763ec4e9
Erik Moqvist <erik.moqvist@gmail.com>
2020-02-02 19:39:25 +0100

    Try to fix benchmark on travis.

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -5,6 +5,8 @@ set -e
 PYTHON="${PYTHON:-python3}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+echo "Python: $PYTHON"
+
 export PYTHONPATH=$SCRIPT_DIR/..
 
 function create_patch() {
