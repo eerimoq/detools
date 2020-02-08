4b5903a49adfdfeb7f569012835e97dbb8fa3795
Erik Moqvist <erik.moqvist@gmail.com>
2020-02-08 16:23:32 +0100

    Version 0.42.0.

--- src/c/detools.h
+++ src/c/detools.h
@@ -60,7 +60,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define PBTOOLS_VERSION                  "0.41.0"
+#define PBTOOLS_VERSION                  "0.42.0"
 
 /* Error codes. */
 #define DETOOLS_OK                              0
